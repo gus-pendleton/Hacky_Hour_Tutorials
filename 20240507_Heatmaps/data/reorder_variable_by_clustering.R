d3eab2e63d7c0ddbc0e46985377c5a6a67ceffbf
gus=pendleton <guspendleton@gmail.com>
2024-05-06 09:40:17 -0400

    Finish lesson plan

--- 20240507_Heatmaps/data/reorder_variable_by_clustering.R
+++ 20240507_Heatmaps/data/reorder_variable_by_clustering.R
@@ -1,4 +1,4 @@
-reorder_variable_by_clustering <- function(data, variable_to_order, response_variable, count_variable, value_fill = NULL){
+reorder_variable_by_clustering <- function(data, variable_to_order, response_variable, count_variable, value_fill = NULL, return_dendro = FALSE){
   
   if(is.null(value_fill)){
   wide <- data[c(variable_to_order, response_variable, count_variable)] %>%
@@ -8,15 +8,21 @@ reorder_variable_by_clustering <- function(data, variable_to_order, response_var
       pivot_wider(names_from = one_of(response_variable), values_from = one_of(count_variable), values_fill = value_fill)
   }
   
-  order_indices <- wide %>%
+   dendrogram <- wide %>%
     dist() %>%
     hclust() %>%
-    as.dendrogram() %>%
+    as.dendrogram() 
+  order_indices <- dendrogram %>%
     order.dendrogram()
 
   data[variable_to_order] <- factor(data[[variable_to_order]], levels = wide[[variable_to_order]][order_indices])
-
+  
+  if(return_dendro){
+    return(list(reord_df = data,
+                dendrogram = dendrogram))
+  }else{
   return(data)
+  }
 }
 
 # Consider if you're then going to group by another group (nesting, essentially)
